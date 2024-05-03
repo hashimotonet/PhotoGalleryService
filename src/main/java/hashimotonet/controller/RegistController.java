10a38014659cd7c934580c4ec694b7cff979887d
Osamu Hashimoto <hashimoto.osamu@gmail.com>
2024-05-03 23:19:23 +0900

    サインアップ画面、及び登録サービス系の作成を進める

---
+++ src/main/java/hashimotonet/controller/RegistController.java
@@ -0,0 +1,46 @@
+/**
+ * 
+ */
+package hashimotonet.controller;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.session.FindByIndexNameSessionRepository;
+import org.springframework.session.Session;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class RegistController {
+
+	Logger log = LogManager.getLogger(RegistController.class);
+
+	@Autowired
+    private FindByIndexNameSessionRepository<? extends Session> sessionRepository;
+	
+	@Autowired
+	HttpServletRequest request;
+
+    @GetMapping("/Regist")
+    public String registEndpoint(@RequestParam("sessionid") String sessionId) {
+        // SessionRepositoryを使用してセッションを取得
+        Session session = sessionRepository.findById(sessionId);
+
+        if (session != null) {
+            // セッションが見つかった場合の処理
+        	String email = (String)session.getAttribute("mailAddress");		 // メールアドレス
+            String accountId = (String) session.getAttribute("accountId");  // アカウント名
+            
+            log.info("Welcome back, " + accountId + "!");
+            return "RegistComplete";
+        } else {
+            // セッションが見つからない場合の処理
+        	log.error("Not found any of same identity.");
+            return "unauthorized";
+        }
+    }
+}
---
+++ src/main/java/hashimotonet/service/RegistrationService.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+package hashimotonet.service;
+
+import org.springframework.stereotype.Service;
+
+/**
+ * 
+ */
+@Service
+public class RegistrationService {
+
+	public boolean execute(String mail, String accountId, String password) {
+		boolean success = false;
+		
+		// TODO DAO の実装（新規作成系）
+		
+		return success;
+	}
+}
